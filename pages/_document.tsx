7916a7af5b40357eb5f44a1ca0fc5280b86851f5
AyRick <aymeric.daniel@isen.yncrea.fr>
2024-02-21 14:22:27 +0100

    favicon.ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,20 +8,14 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="La Galerie photo de la Blood Run"
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="galerie.bloodrun.fr" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="La Galerie photo de la Blood Run"
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
-          <meta
-            name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
+          <meta property="og:title" content="Galerie photo Bloodrun" />
         </Head>
         <body className="bg-black antialiased">
           <Main />
